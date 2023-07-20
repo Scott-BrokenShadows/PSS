f192453cc29520b58164f795528a8742647d2334
KKroz <krozflame@gmail.com>
2023-07-20 07:00:11 -0700

    Analog Controls
    Scott and Andy suggest to revert this to Analog Controls

--- Assets/JT/Scripts/Battle/BattlePlayerControl.cs
+++ Assets/JT/Scripts/Battle/BattlePlayerControl.cs
@@ -4,39 +4,73 @@ using UnityEngine;
 
 public class BattlePlayerControl : MonoBehaviour
 {
+    public float speed;
     public float targetRange = 0.1f;
     public static float _targetRange;
     public static Transform _currentTransform;
-
     private Vector3 target;
+
+    // Player Movement Control
+    Rigidbody2D _rb;
+    Vector2 _movementInput;
+    Vector2 _smoothMovementInput;
+    Vector2 _movementInputSmoothVelocity;
+
     void Awake()
     {
+        _rb = GetComponent<Rigidbody2D>();
         target = transform.position;
         _currentTransform = this.transform;
         _targetRange = targetRange;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        MouseDown();
-        MouseDrag();
+        Move();
     }
 
-    void MouseDown()
+    void Move()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = new Vector3(target.x, target.y);
-        }
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        _movementInput = new Vector2(moveX, moveY).normalized;
+        _smoothMovementInput = Vector2.SmoothDamp(_smoothMovementInput, _movementInput, ref _movementInputSmoothVelocity, 0.1f);
+        _rb.velocity = _smoothMovementInput * speed;
     }
 
-    void MouseDrag()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = new Vector3(target.x, target.y);
-        }
-    }
+    //public float targetRange = 0.1f;
+    //public static float _targetRange;
+    //public static Transform _currentTransform;
+
+    //private Vector3 target;
+    //void Awake()
+    //{
+    //    target = transform.position;
+    //    _currentTransform = this.transform;
+    //    _targetRange = targetRange;
+    //}
+
+    //void Update()
+    //{
+    //    MouseDown();
+    //    MouseDrag();
+    //}
+
+    //void MouseDown()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //        transform.position = new Vector3(target.x, target.y);
+    //    }
+    //}
+
+    //void MouseDrag()
+    //{
+    //    if (Input.GetMouseButton(0))
+    //    {
+    //        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //        transform.position = new Vector3(target.x, target.y);
+    //    }
+    //}
 }
